69f6dd1fa53c6588d87c9e8d67b99d09bac4dac7
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-24 08:11:08 +0600

    brand temp

--- src/Schema/brandSchema.ts
+++ src/Schema/brandSchema.ts
@@ -1,12 +1,13 @@
 import { Schema } from "mongoose";
 import validator from "validator";
-
+const { ObjectId } = Schema.Types;
 const BrandSchema = new Schema({
   name: {
     type: String,
     required: true,
     trim: true,
     unique: true,
+    lowercase: true,
     maxLength: [100, "Name cannot be more than 100 characters"],
   },
   description: {
@@ -18,11 +19,35 @@ const BrandSchema = new Schema({
     type: String,
     required: true,
     trim: true,
+    lowercase: true,
     validate: [validator.isEmail, "Please provide a valid email"],
   },
   website: {
     type: String,
     validate: [validator.isURL, "Please provide a valid website"],
   },
-  
+  location: String,
+  products: [
+    {
+      type: ObjectId,
+      ref: "Product",
+    },
+  ],
+  suppliers: [
+    {
+      name: String,
+      contactNumber: String,
+      id: {
+        type: ObjectId,
+        ref: "Supplier",
+      },
+    },
+  ],
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active",
+  },
+},{
+    timestamps: true,
 });
