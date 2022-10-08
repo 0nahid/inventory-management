579484af8df574573a4626d589be25acea411813
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-09 00:14:17 +0600

    users create api

---
+++ src/controllers/userController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { User } from "../models/userModel";
+
+// create an user
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).send({
+      message: "User created",
+      status: 201,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Internal Server Error",
+      status: 500,
+      error: error,
+    });
+  }
+};
+
+export const userRoute = {
+  createUser,
+};
